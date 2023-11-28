2c693b102c03a3be5a208f7ffc0679d9c2da9ff9
starduststr <starduststr0@gmail.com>
2023-11-29 02:00:10 +0700

    feat: add layout, navbar

---
+++ src/app/navbar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="flex fixed-top bg-blue-800 py-4 px-5 justify-between">
+      <h1 className="text-white text-2xl">
+        BERI<span className="text-blue-400">TAMA</span>
+      </h1>
+      <ul className="flex ml-80">
+        <li className="mr-6 text-white cursor-pointer">
+          <Link href="/">Home</Link>
+        </li>
+        <li className="mr-6 text-white cursor-pointer">
+          <Link href="/news">News</Link>
+        </li>
+      </ul>
+      <Link href="/signup" className="text-white cursor-pointer">
+        Sign Up
+      </Link>
+    </nav>
+  );
+}
